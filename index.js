9e0af28c4d2e5315a339e2daf47d432fc77cead0
ILoveYouDrZaius <iloveyoudrzaius@gmail.com>
2017-07-26 21:49:51 +0200

    Modificado CORS de la api para que angular la pueda consumir

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ function promiseParseStream(){
   });
 }
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.get('/songs', function (req, res) {
   promiseParseStream().then((arraySongs)=>{
     res.send(arraySongs);
